e97c5e57860090634cc6bd77143dee65c34b7a90
Wuao Liu <50629158+Wuao652@users.noreply.github.com>
2020-07-21 22:44:02 +0800

    second commit
    add some evaluate metrics in  test.py
    fix the bug of how to validate the model

--- train.py
+++ train.py
@@ -21,7 +21,8 @@ tf.app.flags.DEFINE_float("max_gradient_norm", 5.0, "Maximum gradient norm.")
 tf.app.flags.DEFINE_boolean("forward_only", False, "Forward Only.")
 tf.app.flags.DEFINE_string("log_dir", "./log", "Log directory")
 tf.app.flags.DEFINE_string("data_path", "./data/convex_hull_5_test.txt", "Data path.")
-tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200, "frequence to do per checkpoint.")
+
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 312, "frequence to do per checkpoint.")
 tf.app.flags.DEFINE_integer("train_epoch", 150, "The train epochs")
 
 FLAGS = tf.app.flags.FLAGS
@@ -36,8 +37,32 @@ def eval(pred, target):
         t2 = pred[i][:tar_length]
         if all(t1 == t2):
             correct_count = correct_count + 1
-    print('Acc: {:.2f}% ({}/{})'.format(correct_count / target.shape[0] * 100, correct_count,
-                                        target.shape[0]))
+    return correct_count
+def eval_valid(data, model, sess, var, trainsummary):
+    inputs, enc_input_weights, outputs, dec_input_weights = \
+        data.get_batch(False)
+    count, correct_count = 0, 0
+    while inputs.shape[0] >= FLAGS.batch_size:
+
+        batch_inputs = inputs[:FLAGS.batch_size]
+        batch_enc_input_weights = enc_input_weights[:FLAGS.batch_size]
+        batch_outputs = outputs[:FLAGS.batch_size]
+        batch_dec_input_weights = dec_input_weights[:FLAGS.batch_size]
+        inputs = inputs[FLAGS.batch_size:]
+        enc_input_weights = enc_input_weights[FLAGS.batch_size:]
+        outputs = outputs[FLAGS.batch_size:]
+        dec_input_weights = dec_input_weights[FLAGS.batch_size:]
+
+        predicted_ids_with_logits, targets = \
+            model.step(sess, batch_inputs, batch_enc_input_weights, batch_outputs, batch_dec_input_weights, var)
+        temp = eval(predicted_ids_with_logits, targets)
+        correct_count = correct_count + temp
+        count = count + 1
+
+    print('Acc: {:.2f}% ({}/{})'.format(correct_count / (count * FLAGS.batch_size) * 100, correct_count,
+                                        count * FLAGS.batch_size))
+    trainsummary.write('Acc: {:.2f}% ({}/{}) \n'.format(correct_count / (count * FLAGS.batch_size) * 100, correct_count,
+                                        count * FLAGS.batch_size))
 def main():
     train_data = DataGenerator(FLAGS, 'train', 40000)
     valid_data = DataGenerator(FLAGS, 'valid', 5000)
@@ -53,7 +78,7 @@ def main():
                                             learning_rate=FLAGS.learning_rate,
                                             max_gradient_norm=FLAGS.max_gradient_norm,
                                             )
-
+    trainsummary = open('trainingsummary.txt', 'w+')
     with tf.Session() as sess:
         writer = tf.summary.FileWriter(FLAGS.log_dir + '/train', sess.graph)
         ckpt = tf.train.get_checkpoint_state(FLAGS.log_dir)
@@ -63,6 +88,7 @@ def main():
         else:
             print("Created model with fresh parameters.")
             sess.run(tf.global_variables_initializer())
+        print('start!!!!!!!!!!!!!!!!!')
         step_time = 0.0
         loss = 0.0
         current_step = 0
@@ -83,29 +109,18 @@ def main():
                 with sess.as_default():
                     gstep = my_model.global_step.eval()
                 print("global step %d step-time %.2f loss %.2f" % (gstep, step_time, loss))
+                trainsummary.write('Epoch %d \n' % (current_step/FLAGS.steps_per_checkpoint))
+                trainsummary.write("global step %d step-time %.2f loss %.2f \n" % (gstep, step_time, loss))
                 ####
-                inputs, enc_input_weights, outputs, dec_input_weights = \
-                    valid_data.get_batch(True)
-                predicted_ids_with_logits, targets = \
-                    my_model.step(sess, inputs, enc_input_weights, outputs, dec_input_weights, train_flag_var)
+                eval_valid(valid_data, my_model, sess, train_flag_var, trainsummary)
 
-                eval(predicted_ids_with_logits, targets)
-
-                # Write summary
                 writer.add_summary(summary, gstep)
-                # Randomly choose one to check
-                # sample = np.random.choice(FLAGS.batch_size, 1)[0]
-                # print('=' *20)
-                # print(predicted_ids_with_logits[1].reshape(FLAGS.batch_size, -1))
-                # print(targets)
-                # print("=" * 20)
-                # print("Predict: " + str(np.array(predicted_ids_with_logits[1][sample]).reshape(-1)))
-                # print("Target : " + str(targets[sample]))
-                # print("=" * 20)
                 checkpoint_path = os.path.join(FLAGS.log_dir, "wdp.ckpt")
                 my_model.saver.save(sess, checkpoint_path, global_step=my_model.global_step)
                 step_time, loss = 0.0, 0.0
                 train_flag_var = False
+
+    trainsummary.close()
 
 if __name__ == '__main__':
     main()
